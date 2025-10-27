c6e56080944f5688c62d4453d347c753b7e8478e
Linus Edström <linusedstrom@gmail.com>
2025-10-27 09:52:11 +0100

    lägger till member

--- Main.java
+++ Main.java
@@ -1,7 +1,30 @@
 package com.Linus;
 
+
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+        boolean goMeny = true;
+
+        while(goMeny){}
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        int val;
+
+        switch(val)
+            {
+            case 1:
+                System.out.println();
+            }
+
+
     }
 }
---
+++ Member.java
@@ -0,0 +1,39 @@
+package com.Linus;
+
+public class Member {
+
+    //attribut
+
+    private int id;
+    private String name;
+    private String status;
+
+    //konstruktorer
+
+    public Member(){}
+
+    public Member(int id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public void setStatus(String status){
+        this.status = status;
+    }
+    public void history(){}
+}
