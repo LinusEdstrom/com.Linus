bb18455f61e09e2c5bc94ce99f4ad50a43d24f89
Linus Edström <linusedstrom@gmail.com>
2025-10-27 11:37:38 +0100

    la till attribut i SubDvds klasserna

--- entity/Action.java
+++ entity/Action.java
@@ -1,4 +1,15 @@
 package com.Linus.entity;
 
-public class Action {
+public class Action extends Dvd {
+
+    //attribute
+
+    private int explosions;
+    private int coolOneliners;
+
+    public Action(String name, int price, String title, String description, int lenght, int explosions, int coolOneliners){
+        super(name, price, title, description, lenght);
+        this.explosions = explosions;
+        this.coolOneliners = coolOneliners;
+    }
 }
--- entity/Horror.java
+++ entity/Horror.java
@@ -1,4 +1,16 @@
 package com.Linus.entity;
 
-public class Horror {
+public class Horror extends Dvd {
+
+    //attribute
+
+    private int allowedFromAge;
+    private int screamScenes;
+
+    public Horror(String name, int price, String title, String description, int lenght, int allowedFromAge, screamScenes) {
+        super(name, price, title, description, lenght);
+        this.allowedFromAge = allowedFromAge;
+        this.screamScenes = screamScenes;
+        
+    }
 }
